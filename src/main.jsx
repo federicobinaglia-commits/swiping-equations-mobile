757a41d0962951b936acef756e4aaf12dbda17a7
federicobinaglia-commits <federico.binaglia@iisgubbio.edu.it>
2026-02-20 12:40:22 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import {createRoot} from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+// Ho tolto il "!" che bloccava il browser
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
